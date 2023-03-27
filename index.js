2e0eaa2f88518194fe7ad615846051398f288495
rafig184 <rafiganon@gmail.com>
2023-03-27 11:05:26 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,118 @@
+const DOM = {
+    searchInput: document.querySelector("#searchInput"),
+    searchButton: document.querySelector("#searchButton"),
+    content: document.querySelector("#content"),
+}
+
+function init() {
+    DOM.searchButton.addEventListener("click", getMovieHandler)
+}
+init();
+
+async function getMovieHandler() {
+    try {
+        showLoader();
+        const result = await getMovie(DOM.searchInput.value);
+        for (let index = 0; index < result.length; index++) {
+            const element = result[index];
+            const movies = element.Title;
+            const poster = element.Poster;
+            const id = element.imdbID;
+            drawMovies(movies, poster, id);
+        }
+
+        DOM.searchInput.value = ""
+    } catch (error) {
+        console.log(error);
+        DOM.searchInput.value = ""
+        swal({
+            title: "Something went wrong!",
+            text: "Contact admin",
+            icon: "error",
+        });
+    } finally {
+        removeLoader();
+    }
+}
+
+
+
+
+function drawMovies(title, poster, id) {
+    const div = document.createElement("div");
+    div.classList.add("contentDiv")
+    div.addEventListener("click", getMovieDescription)
+    div.id = id;
+    const img = getImg(poster);
+    const h6 = document.createElement("h6");
+    h6.innerText = title;
+    const movieId = document.createElement("p")
+    movieId.innerText = `Movie ID: ${id}`
+
+    div.append(img, h6, movieId);
+    DOM.content.append(div);
+}
+
+function drawMoviesWithPlot(title, poster, plot) {
+    const div = document.createElement("div");
+    div.classList.add("contentDivWithPlot")
+    const img = getImg(poster);
+    const h4 = document.createElement("h4");
+    h4.innerText = title;
+    const p = document.createElement("p");
+    p.innerText = plot;
+
+    div.append(img, h4, p);
+    DOM.content.append(div);
+}
+
+async function getMovieDescription(movieDiv) {
+    try {
+        showLoader();
+        const result = await getMovieId(movieDiv.currentTarget.id);
+
+        const movies = result.Title;
+        const poster = result.Poster;
+        const plot = result.Plot;
+
+        drawMoviesWithPlot(movies, poster, plot);
+
+    } catch (error) {
+        console.log(error);
+        swal({
+            title: "Something went wrong!",
+            text: "Contact admin",
+            icon: "error",
+        });
+    } finally {
+        removeLoader();
+    }
+}
+
+
+async function getMovie(title) {
+    const result = await fetch(`https://www.omdbapi.com/?s=${title}&apikey=adc1d2a6`);
+    const json = await result.json();
+    return json.Search;
+}
+
+async function getMovieId(id) {
+    const result = await fetch(`https://www.omdbapi.com/?i=${id}&apikey=adc1d2a6`);
+    const json = await result.json();
+    return json;
+}
+
+function showLoader() {
+    DOM.content.innerHTML = "";
+    const loader = document.createElement("div");
+    loader.id = "searchLoader";
+    loader.classList.add("spinner-border");
+    DOM.content.append(loader);
+}
+
+function removeLoader() {
+    const loader = document.querySelector("#searchLoader");
+    if (loader) {
+        loader.remove();
+    }
+}
---
+++ ui-components/image.js
@@ -0,0 +1,7 @@
+function getImg(src) {
+  const img = document.createElement("img");
+  img.height = 300;
+  img.width = 200;
+  img.src = src;
+  return img;
+}
